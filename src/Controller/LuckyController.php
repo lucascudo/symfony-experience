14f2f3c340579644286c045f7d3ea0090cd02371
Lucas Teixeira Rocha <lucasrochabr@outlook.com>
2018-02-06 12:37:37 -0300

    Adds Session management

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -1,9 +1,11 @@
 <?php // src/Controller/LuckyController.php
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 class LuckyController extends Controller
 {
@@ -17,12 +19,17 @@ class LuckyController extends Controller
       *     }
       * )
       */
-    public function number($userNumber = 0)
+    public function number(SessionInterface $session, $userNumber = 0)
     {
-        $responseObject = [
-          'userNumber' => $userNumber,
-          'randomNumber' => mt_rand(0, 100)
-        ];
-        return $this->render('lucky/number.html.twig', $responseObject);
+      $request = Request::createFromGlobals();
+      // use a default value if the attribute doesn't exist
+      $userAgent = $session->get('user-agent', $request->headers->get('user-agent'));
+      // store an attribute for reuse during a later user request
+      $session->set('user-agent', $userAgent);
+      $responseObject = [
+        'userNumber' => $userNumber,
+        'randomNumber' => mt_rand(0, 100)
+      ];
+      return $this->render('lucky/number.html.twig', $responseObject);
     }
 }
